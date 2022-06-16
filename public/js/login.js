138cdd4ac6e9aac81efe9a7c3e4b1008dc82f153
Jaryodd Carter <jaryodd@gmail.com>
2022-06-16 10:02:30 -0600

    if statement

--- public/js/login.js
+++ public/js/login.js
@@ -3,18 +3,32 @@ const loginFormHandler = async function(event) {
 
   const emailEl = document.querySelector('#email');
   const passwordEl = document.querySelector('#password');
+  const usernameEL = document.querySelector('#username');
 
   const response = await fetch('/api/user/login', {
     method: 'POST',
     body: JSON.stringify({
       username: emailEl.value,
       password: passwordEl.value,
+      username: usernameEL.value,
     }),
     headers: { 'Content-Type': 'application/json' },
   });
 
-  if (response.ok) {
-    console.log("SUCCESS")
+ if (email === "") {
+    displayMessage("error", "Email cannot be blank");
+  } else if (password === "") {
+    displayMessage("error", "Password cannot be blank");
+ } else if (username === "") {
+   displayMessage("Username cannot be blank")
+ }
+ else {
+    displayMessage("success", "Registered successfully");
+
+
+
+  // if (response.ok) {
+  //   console.log("SUCCESS")
 //     document.location.replace('/dashboard');
 //   } else {
 //     alert('Failed to login');
